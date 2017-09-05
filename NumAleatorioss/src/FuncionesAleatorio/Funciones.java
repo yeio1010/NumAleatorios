f86dfe262620560ba2352c6cc4da754f5589707c
Dicita <Dicita@DESKTOP-48HJ855>
2017-09-04 23:41:15 -0500

    Archivo final

---
+++ NumAleatorioss/src/FuncionesAleatorio/Funciones.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FuncionesAleatorio;
+
+/**
+ *
+ * @author Dicita
+ */
+public class Funciones {
+    public static double FuncionCambio(double n){
+        return (n+2)/Math.pow(n, 2);
+     }
+    public static double FuncionSalida(double n){
+         return n/33;
+     }
+    
+}
---
+++ NumAleatorioss/src/numaleatorioss/NumAleatorioss.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numaleatorioss;
+import FuncionesAleatorio.Funciones;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dicita
+ */
+public class NumAleatorioss {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n=0, aux=0;
+        double [] s=new double[5],u= new double[5];
+        
+        System.out.println("Digite el valor semilla\n");
+        Scanner leer= new Scanner(System.in);
+        int d =leer.nextInt();
+        
+        s[0]=d;
+        while(aux< 4){
+            n++;
+            s[n]=Funciones.FuncionCambio(s[n-1]);
+            u[n-1]=Funciones.FuncionSalida(s[n]); 
+            aux++;
+    }
+    System.out.println("\nLos numeros Aleatorios generados son:\n");
+     System.out.println(Arrays.toString(s) + "\n"+ Arrays.toString(u)+"\n");
+}
+}
